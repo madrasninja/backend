a4c7e7eb00c209247c3860bb83f2401f0f0975e3
karthisgk <karthisg.sg@gmail.com>
2019-02-01 06:57:56 +0530

    dbase collection created

---
+++ application/config/db/collections.js
@@ -0,0 +1,130 @@
+db.createCollection("user", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "First_Name", "Mobile_Number", "Email_Id", "User_Type" ],
+         properties: {
+            First_Name: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            Mobile_Number: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            Email_Id: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            User_Type: {
+               enum: [ 0,1,2,3 ],/* 0=superAdmin,1=admin,2=labour,3=customer*/
+               description: "can only be one of the enum values and is required"
+            }
+         }
+      }
+   }
+})
+
+
+db.createCollection("booking", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "User_ID", "Locality_ID", "Service_Type_ID", "Status_ID", "Payment_Status", "Session_Time.From","Session_Time.To" ],
+         properties: {
+            User_ID: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            Locality_ID: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            Service_Type_ID: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            Status_ID: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            Payment_Status: {
+               enum: [ 0,1,2,3 ],/*0=pending,1=success,2=cancel,3=failed*/
+               description: "can only be one of the enum values and is required"
+            },
+            "Session_Time.From" : {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            "Session_Time.To" : {
+               bsonType: "string",
+               description: "must be a string and is required"
+            }
+         }
+      }
+   }
+})
+
+db.createCollection("status", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "name" ],
+         properties: {
+            name: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            }
+         }
+      }
+   }
+})
+
+db.createCollection("locality", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "name" ],
+         properties: {
+            name: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            }
+         }
+      }
+   }
+})
+
+db.createCollection("service_type", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "name" ],
+         properties: {
+            name: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            }
+         }
+      }
+   }
+})
+
+const statusData = [
+   {name: 'New'},
+   {name: 'Processing'},
+   {name: 'Request Assigned'},
+   {name: 'Work In-Progress'},
+   {name: 'Completed'}
+];
+
+const service_typeData = [
+   {name: 'Full House cleaning'},
+   {name: 'Kitchen Cleaning'},
+   {name: 'Restroom Cleaning'},
+   {name: 'Carpet Cleaning'},
+   {name: 'Sofa Cleaning'},
+   {name: 'Marble Polishing'}
+];
+
+const localityData = [];
