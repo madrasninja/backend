983b0e3898eaebc8322a212e9565a4852a573961
karthisgk <karthisg.sg@gmail.com>
2019-03-08 20:27:04 +0530

    cxc

--- application/app.js
+++ application/app.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Headers', 'token');
+    //res.header('Access-Control-Allow-Headers', 'token');
     next();
 });
 
