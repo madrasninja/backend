eecea26de1b829f7df68e6a578dd6e3615548c9c
karthisgk <karthisg.sg@gmail.com>
2019-03-10 18:52:11 +0530

    asd

--- application/routes/index.js
+++ application/routes/index.js
@@ -64,6 +64,9 @@ function Routes(app){
 
 	app.use(function(req, res, next){
 
+		res.header('Access-Control-Allow-Origin', '*');
+	    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	    res.header('Access-Control-Allow-Headers', 'Content-Type');
 		res.header('Access-Control-Allow-Headers', 'token');
 
 		if(!req.headers.hasOwnProperty('token')){
