cdb50b811438cb571d7ebd8f118255490d1e4c02
karthisgk <karthisg.sg@gmail.com>
2019-04-17 17:35:11 +0530

    safcv

--- application/app.js
+++ application/app.js
@@ -31,8 +31,8 @@ app.use(session({
     maxAge: appConfig.session_time
   }
 }));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
